0dac713a912c86645877c0a8c0cbc05b34f04e57
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2024-04-11 16:16:48 +0200

    feat: active route link added to navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <nav class="mx-2 my-2 sm:mr-0 w-full sm:w-1/6 flex sm:flex-col gap-2">
-    <RouterLink
-      class="px-4 py-2 rounded border-2 border-black transition-all hover:bg-gray-200"
-      to="/"
-      >Task 1</RouterLink
-    >
-    <RouterLink
-      class="px-4 py-2 rounded border-2 border-black hover:bg-gray-200"
-      to="/task2"
-      >Task 2</RouterLink
-    >
-    <RouterLink
-      class="px-4 py-2 rounded border-2 border-black hover:bg-gray-200"
-      to="/task3"
-      >Task 3</RouterLink
-    >
-  </nav>
+  <Navbar />
   <main class="m-2 sm:m-0 grow sm:w-5/6">
     <suspense>
       <RouterView />
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import Task2Vue from "./Task2.vue";
 import Task3Vue from "./Task3.vue";
 
 const routes = [
-  { path: "/", component: Task1Vue },
-  { path: "/task2", component: Task2Vue },
-  { path: "/task3", component: Task3Vue }
+  { path: "/", name: "task1", component: Task1Vue },
+  { path: "/task2", name: "task2", component: Task2Vue },
+  { path: "/task3", name: "task3", component: Task3Vue }
 ];
 
 export const router = createRouter({
